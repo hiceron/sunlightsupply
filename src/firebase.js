47557246e24f6cfcffa23d0a4bb68e13671cb141
hiceron <hiceron@gmail.com>
2024-10-21 16:40:20 +0700

    Views

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA8hE_Ps-wUpk5DDm-fO3uMnvj8GnxJabU",
-  authDomain: "sunlightsupply-a57d0.firebaseapp.com",
-  projectId: "sunlightsupply-a57d0",
-  storageBucket: "sunlightsupply-a57d0.appspot.com",
-  messagingSenderId: "399028063207",
-  appId: "1:399028063207:web:3b3991136ed26318eb4b42",
-  measurementId: "G-7C7R6S7WPR"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain:  process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:  process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
